920afd9846156e6cb946e75323e509ba5d200682
carRace <osigotojobu@yahoo.co.jp>
2021-08-30 20:06:02 +0900

    overスクリプト
    overスクリプト追加

---
+++ Assets/over.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class over : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.gameObject.GetComponent<Text>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void Lose(){
+         this.gameObject.GetComponent<Text>().enabled = true;
+
+    }
+}
